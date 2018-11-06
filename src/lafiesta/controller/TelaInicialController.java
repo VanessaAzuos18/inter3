7fb72795fbae2604a656975c4a42c17b947e4376
Matheus Vassoler <matheus.s.vassoler@gmail.com>
2018-11-06 17:27:46 -0200

    Altera a TelaInicial e adiciona a tela IniciarFesta

--- src/lafiesta/controller/TelaInicialController.java
+++ src/lafiesta/controller/TelaInicialController.java
@@ -1,23 +1,47 @@
 package lafiesta.controller;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class TelaInicialController extends Application {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TelaInicialController implements Initializable {
 
     private String nomeUsuario;
 
+
     @FXML
     private Label nome;
+    @FXML
+    private AnchorPane menu;
+    @FXML
+    private Button menuBotao;
+
+    //
 
 
     public void setNome(String nomeUsuario){
         this.nome.setText(nomeUsuario);
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void handleMenu(ActionEvent e) {
+        if(menu.isVisible() == false) {
+            menu.setVisible(true);
+        } else {
+            menu.setVisible(false);
+        }
     }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        menu.setVisible(false);
+    }
+
 }
