912244c3b24d4166378ee36a8a7cfe5db2203787
Erick Gomes <erickgvieira@hotmail.com>
2018-11-12 17:06:12 -0200

    lista de festas + remover festa

--- src/lafiesta/controller/TelaInicialController.java
+++ src/lafiesta/controller/TelaInicialController.java
@@ -1,6 +1,5 @@
 package lafiesta.controller;
 
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,12 +8,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import lafiesta.model.dao.FestaDAO;
+import lafiesta.model.domain.Festa;
 import lafiesta.model.domain.Usuario;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,17 +28,32 @@ public class TelaInicialController implements Initializable {
 
     private String nomeUsuario;
 
-
     @FXML
     private Text nome;
+    @FXML
+    private Label tipo;
     @FXML
     private AnchorPane menu;
     @FXML
     private Button menuBotao;
     @FXML
     private Text txtSair;
+    @FXML
+    private TableView<Festa> tabela;
+    @FXML
+    private TableColumn<Festa, Integer> id;
+    @FXML
+    private TableColumn<Festa, String> nome_festa;
+    @FXML
+    private TableColumn<Festa, String> local;
+    @FXML
+    private TableColumn<Festa, String> data;
+    @FXML
+    private TableColumn<Festa, Integer> convidados;
 
     private Usuario usuario;
+
+    FestaDAO festaDAO = new FestaDAO();
 
     public void setNome(String nomeUsuario){
         this.nome.setText(nomeUsuario);
@@ -48,9 +68,24 @@ public class TelaInicialController implements Initializable {
     }
 
     public void setUsuario(Usuario usuario) {
+
         this.usuario = usuario;
         nome.setText(usuario.getNome());
-        System.out.println(usuario.getTipo());
+        if(usuario.getTipo() == 1) tipo.setText("Cliente");
+        else tipo.setText("Fornecedor / Prestador");
+
+        id.setCellValueFactory(
+                new PropertyValueFactory<>("id"));
+        nome_festa.setCellValueFactory(
+                new PropertyValueFactory<>("nome_festa"));
+        data.setCellValueFactory(
+                new PropertyValueFactory<>("Data"));
+        local.setCellValueFactory(
+                new PropertyValueFactory<>("Local"));
+        convidados.setCellValueFactory(
+                new PropertyValueFactory<>("Convidados"));
+
+        tabela.setItems(festaDAO.carregaFestas(usuario.getId()));
     }
 
     @FXML
@@ -62,7 +97,7 @@ public class TelaInicialController implements Initializable {
 
         stage.setTitle("Login");
         stage.setScene(scene);
-
+        stage.setResizable(false);
         stage.show();
 
         txtSair.getScene().getWindow().hide();
@@ -73,4 +108,11 @@ public class TelaInicialController implements Initializable {
         menu.setVisible(false);
     }
 
+    public void remover(ActionEvent e) {
+        Festa festaRemover = tabela.getSelectionModel().getSelectedItem();
+        if(festaDAO.remover(festaRemover.getId(), usuario.getId()))
+            tabela.setItems(festaDAO.carregaFestas(usuario.getId()));
+        else
+            JOptionPane.showMessageDialog(null, "Erro ao remover a festa!");
+    }
 }
---
+++ src/lafiesta/model/dao/FestaDAO.java
@@ -0,0 +1,57 @@
+package lafiesta.model.dao;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import lafiesta.model.database.BDFabricaConexao;
+import lafiesta.model.domain.Festa;
+import lafiesta.model.domain.Usuario;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FestaDAO {
+    private Connection connection;
+
+    public FestaDAO() {
+        connection = BDFabricaConexao.getConnection();
+    }
+
+    public ObservableList<Festa> carregaFestas(int id) {
+        ObservableList<Festa> festas = FXCollections.observableArrayList();
+
+        try {
+            String sql = "SELECT * FROM festa where id_usuario = " + id + " order by id;";
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet rset = stmt.executeQuery();
+            while(rset.next()){
+                festas.add( new Festa(rset.getInt("id"), rset.getString("nome_festa"),
+                        rset.getString("data"),rset.getString("local"), rset.getInt("convidados")));
+            }
+            return festas;
+        } catch(SQLException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    public boolean remover(int index, int id){
+        try {
+            String sql = null;
+
+            sql = "delete from festa where id = " + index + " and id_usuario = " + id;
+
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.executeUpdate();
+
+            return true;
+        } catch(SQLException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+}
