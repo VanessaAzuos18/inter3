381018be8168199faf5581826e9c767feafe0219
Erick Gomes <erickgvieira@hotmail.com>
2018-11-22 21:14:55 -0200

    Tela de cadastro de produtos e serviços.

---
+++ src/lafiesta/controller/CadastroServProdController.java
@@ -0,0 +1,22 @@
+package lafiesta.controller;
+
+import javafx.event.ActionEvent;
+import lafiesta.model.domain.Usuario;
+
+import java.io.IOException;
+
+public class CadastroServProdController {
+    private Usuario usuario;
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public void handleCadastrar(ActionEvent e) throws IOException {
+
+    }
+
+    public void handleVoltar(ActionEvent e) throws IOException {
+
+    }
+}
